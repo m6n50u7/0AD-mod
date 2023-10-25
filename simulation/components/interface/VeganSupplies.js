c68a9ce255a481a9538011711a22f1952a9205b5
man_s_our <34793391+m6n50u7@users.noreply.github.com>
2023-10-25 07:52:38 +0100

    Add files via upload

---
+++ simulation/components/interface/VeganSupplies.js
@@ -0,0 +1,7 @@
+Engine.RegisterInterface("VeganSupplies");
+
+/**
+ * Message of the form { "from": number, "to": number }
+ * sent from Health component whenever health changes.
+ */
+Engine.RegisterMessageType("VeganSuppliesChanged");
