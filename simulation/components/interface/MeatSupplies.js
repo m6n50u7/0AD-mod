d3e2f83e300f9606d0850e394ab29a8d03c838ce
man_s_our <34793391+m6n50u7@users.noreply.github.com>
2023-10-27 20:25:32 +0100

    fix

--- simulation/components/interface/MeatSupplies.js
+++
@@ -1,7 +0,0 @@
-Engine.RegisterInterface("MeatSupplies");
-
-/**
- * Message of the form { "from": number, "to": number }
- * sent from Health component whenever health changes.
- */
-Engine.RegisterMessageType("MeatSuppliesChanged");
